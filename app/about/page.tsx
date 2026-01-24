74228e07a2e4d128e735d7306b80bbd6de779b2b
Ownerclone <info@ownerclone.com>
2026-01-24 14:27:20 -0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import React from 'react';
-import Navigation from '../components/Navigation';
 import Link from 'next/link';
 
 export default function AboutPage() {
   return (
     <main className="min-h-screen bg-[#0a0a0a] text-white relative overflow-hidden">
-      <Navigation />
       
       {/* Animated background orbs */}
       <div className="fixed inset-0 overflow-hidden pointer-events-none">
