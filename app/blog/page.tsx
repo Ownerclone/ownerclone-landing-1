18ad805d00f6ea1f8cd31b830d35574e5a1c0c07
Ownerclone <info@ownerclone.com>
2026-01-24 14:28:22 -0500

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from 'next'
 import Link from 'next/link'
-import Navigation from '../components/Navigation'
 import { FaClock, FaUser } from 'react-icons/fa'
 
 export const metadata: Metadata = {
@@ -45,8 +44,6 @@ export default async function Blog() {
         <div className="absolute top-1/4 -left-48 w-96 h-96 bg-[#a855f7] rounded-full mix-blend-screen filter blur-[128px] opacity-20 animate-pulse"></div>
         <div className="absolute bottom-1/4 -right-48 w-96 h-96 bg-[#38bdf8] rounded-full mix-blend-screen filter blur-[128px] opacity-20 animate-pulse" style={{animationDelay: '2s'}}></div>
       </div>
-
-      <Navigation />
 
       {/* Hero Section */}
       <section className="relative pt-32 pb-16 px-4">
