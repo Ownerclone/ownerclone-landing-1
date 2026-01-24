611241700aee4e73cc7efe72a3277980753ed9e9
Ownerclone <info@ownerclone.com>
2026-01-24 14:29:28 -0500

    Update page.tsx

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from 'next'
 import Link from 'next/link'
-import Navigation from '../components/Navigation'
 import { FaCheck } from 'react-icons/fa'
 
 export const metadata: Metadata = {
@@ -17,8 +16,6 @@ export default function Pricing() {
         <div className="absolute top-1/4 -left-48 w-96 h-96 bg-[#10b981] rounded-full mix-blend-screen filter blur-[128px] opacity-20 animate-pulse"></div>
         <div className="absolute bottom-1/4 -right-48 w-96 h-96 bg-[#38bdf8] rounded-full mix-blend-screen filter blur-[128px] opacity-20 animate-pulse" style={{animationDelay: '2s'}}></div>
       </div>
-
-      <Navigation />
 
       {/* Hero Section */}
       <section className="relative pt-32 pb-16 px-4">
