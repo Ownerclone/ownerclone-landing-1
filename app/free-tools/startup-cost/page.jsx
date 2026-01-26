5df3369d2aa2dce7e32814e2c8d9ee1916a0a7f8
Ownerclone <info@ownerclone.com>
2026-01-26 16:14:36 -0500

    Update page.jsx

--- app/free-tools/startup-cost/page.jsx
+++ app/free-tools/startup-cost/page.jsx
@@ -144,15 +144,6 @@ export default function StartupCostCalculator() {
 
   return (
     <div className="min-h-screen relative">
-  <div 
-    className="fixed inset-0 z-0"
-    style={{
-      backgroundImage: 'url(/bg-glow.svg)',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat'
-    }}
-  />
 
   <div className="relative z-10 container mx-auto px-4 pt-24 pb-8 max-w-4xl">
         {/* Header */}
